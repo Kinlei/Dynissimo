95493e39ed46b835eb99503792a0f97af4a655a0
Kinlei <61442909+Kinlei@users.noreply.github.com>
2021-09-14 15:04:22 +0100

    Update StratRecorder.lua

--- Scripts/StratRecorder.lua
+++ Scripts/StratRecorder.lua
@@ -1,16 +1,23 @@
-local StratName = "SoloMoltenShotgun";
+local StratName = "EventFarmACEOnly";
 local APIVar = "TDS";
-local Map, Mode = "Marshlands", "Molten";
-local Towers = {"\"Shotgunner\"", "\"Paintballer\"", "\"Farm\"", "\"Ranger\"", "\"Commander\""}
+local Map, Mode, Type = "Outpost 32", "", "Event";
+--[[ 
+    MAP: Name of Map
+    MODE: Modes - [Normal, Molten, Fallen]
+    TYPE: Types - [Survival, Hardcore, Event]
+]]
+local Towers = {"\"Ace Pilot\"", "nil", "nil", "nil", "nil"};
+local Extension = ".lua";
+local FileName = (StratName..Extension);
 
 local OldIndex, OldNamecall = nil, nil;
 
-writefile(StratName..".txt", string.format([[
+writefile(FileName, string.format([[
 local %s = loadstring(game:HttpGet("https://pastebin.com/raw/JCNCcYBr", true))()
 %s:Loadout(%s, %s, %s, %s, %s)
-%s:Map("%s", true, "Survival")
+%s:Map("%s", true, "%s")
 %s:Mode("%s")
-]], APIVar, APIVar, Towers[1], Towers[2], Towers[3], Towers[4], Towers[5], APIVar, Map, APIVar, Mode));
+]], APIVar, APIVar, Towers[1], Towers[2], Towers[3], Towers[4], Towers[5], APIVar, Map, Type, APIVar, Mode));
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
 local State = ReplicatedStorage.State;
@@ -31,18 +38,18 @@ local Events = {
             local Position = PositionData.Position;
             local CurWave = Wave.Value;
             local TM, TS = Convert(CurTime.Value);
-            appendfile(StratName..".txt", string.format("%s:Place(\"%s\", %f, %f, %f, %d, %d, %d)\n", APIVar, Tower, Position.X, Position.Y, Position.Z, CurWave, TM, TS));
+            appendfile(FileName, string.format("%s:Place(\"%s\", %f, %f, %f, %d, %d, %d)\n", APIVar, Tower, Position.X, Position.Y, Position.Z, CurWave, TM, TS));
         end;
         ["Sell"] = function(Info)
             local Index = table.find(GameTowers:GetChildren(), Info.Troop);
             local TM, TS = Convert(CurTime.Value);
-            appendfile(StratName..".txt", string.format("%s:Sell(%d, %d, %d, %d)\n", APIVar, Index, Wave.Value, TM, TS));
+            appendfile(FileName, string.format("%s:Sell(%d, %d, %d, %d)\n", APIVar, Index, Wave.Value, TM, TS));
         end;
         ["Upgrade"] = {
             ["Set"] = function(Troop)
                 local Index = table.find(GameTowers:GetChildren(), Troop.Troop);
                 local TM, TS = Convert(CurTime.Value);
-                appendfile(StratName..".txt", string.format("%s:Upgrade(%d, %d, %d, %d)\n", APIVar, Index, Wave.Value, TM, TS));
+                appendfile(FileName, string.format("%s:Upgrade(%d, %d, %d, %d)\n", APIVar, Index, Wave.Value, TM, TS));
             end;
         };
         ["Abilities"] = {
@@ -50,14 +57,14 @@ local Events = {
                 local Troop, Ability = Info.Troop, Info.Name;
                 local Index = table.find(GameTowers:GetChildren(), Troop);
                 local TM, TS = Convert(CurTime.Value);
-                appendfile(StratName..".txt", string.format("%s:Ability(%d, \"%s\", %d, %d, %d)\n", APIVar, Index, Ability, Wave.Value, TM, TS));
+                appendfile(FileName, string.format("%s:Ability(%d, \"%s\", %d, %d, %d)\n", APIVar, Index, Ability, Wave.Value, TM, TS));
             end;
         }
     },
     ["Waves"] = {
         ["Skip"] = function()
             local TM, TS = Convert(CurTime.Value);
-            appendfile(StratName..".txt", string.format("%s:Skip(%d, %d, %d)\n", APIVar, Wave.Value, TM, TS));
+            appendfile(FileName, string.format("%s:Skip(%d, %d, %d)\n", APIVar, Wave.Value, TM, TS));
         end;
     }
 }
@@ -73,13 +80,23 @@ OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(...)
                 local Select = table.remove(Args, 1);
                 local Current = Events[Select];
                 if (Current) then
-                    Current = Current[table.remove(Args, 1)];
+                    local Logs = {};
+                    local Next = table.remove(Args, 1);
+                    table.insert(Logs, Next);
+                    Current = Current[Next];
                     while (typeof(Current) == "table") do
-                        Current = Current[table.remove(Args, 1)];
+                        Next = table.remove(Args, 1);
+                        table.insert(Logs, Next);
+                        Current = Current[Next];
                     end;
                     if (typeof(Current) == "function") then
                         local a, b = pcall(Current, unpack(Args));
-                        warn(a, b);
+                        warn(string.format("[A] %s [B] %s", tostring(a), tostring(b)));
+                        if (not a) then
+                            print("-- DEBUG --");
+                            print(unpack(Logs));
+                            print("-- DEBUG --");
+                        end;
                     end;
                 end;
             end)();
